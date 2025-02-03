96a0da3f8eaaca4291d01c8b4bb5a68beafea878
Figueiredo Sebastiao <47941@alunos.upt.pt>
2025-02-03 21:54:33 +0000

    mudanças

--- R_Equipa16/src/main/java/com/upt/lp/R_Equipa16/REquipa16Application.java
+++ R_Equipa16/src/main/java/com/upt/lp/R_Equipa16/REquipa16Application.java
@@ -1,13 +1,11 @@
-package com.upt.lp.R_Equipa16;
+package com.upt.lp.R_Equipa16; // Deve corresponder ao caminho real do ficheiro
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class REquipa16Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(REquipa16Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(REquipa16Application.class, args);
+    }
 }
